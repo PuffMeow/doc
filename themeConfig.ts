a8da45e0992e1f0425cfe7a546e5529016529ca7
JiquanWang99 <951873902@qq.com>
2023-01-11 22:51:17 +0800

    chore: update git

--- themeConfig.ts
+++ themeConfig.ts
@@ -6,11 +6,13 @@ export default {
     '/backend/': getSideBarConfig('backend'),
     '/rust/': getSideBarConfig('rust'),
     '/algorithm/': getSideBarConfig('algorithm'),
+    '/computer_base/': getSideBarConfig('computer_base'),
   },
   nav: [
     { text: '前端', link: '/frontend/' },
     { text: '服务端', link: '/backend/' },
     { text: 'Rust', link: '/rust/' },
     { text: '算法学习', link: '/algorithm/' },
+    { text: '计算机基础', link: '/computer_base/' },
   ],
 };
