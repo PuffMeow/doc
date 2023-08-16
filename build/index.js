bca9d8b428d82a04ee887d9bdcb2c984320bb27d
JiquanWang99 <951873902@qq.com>
2023-08-16 23:44:39 +0800

    feat: 增加文件目录监听器

---
+++ build/index.js
@@ -0,0 +1,36 @@
+const watch = require('glob-watcher');
+const { spawn } = require('child_process');
+const killPort = require('kill-port');
+const watcher = watch(['docs/**/*.md', 'utils']);
+
+const npmCmd = process.platform === 'win32' ? 'npm.cmd' : 'npm'; // 跨平台处理npm命令
+let webpackProcess = spawn(npmCmd, ['run', 'dev'], {
+  shell: true, // 设置为true以运行shell命令
+  stdio: 'inherit', // 将子进程的输入输出与父进程绑定，使其共享终端
+});
+
+webpackProcess.on('close', (code) => {
+  console.log(`Webpack 进程退出 code: ${code}`);
+});
+
+const handler = (path, stat) => {
+  console.log(path);
+  // 杀死之前的Webpack进程
+  webpackProcess.kill();
+  const defaultWebpackPort = 5173;
+  killPort(defaultWebpackPort).then(() => {
+    console.log(`端口 ${defaultWebpackPort} 关闭.`);
+    webpackProcess = spawn(npmCmd, ['run', 'dev'], {
+      shell: true, // 设置为true以运行shell命令
+      stdio: 'inherit', // 将子进程的输入输出与父进程绑定，使其共享终端
+    });
+  });
+};
+
+watcher
+  .on('add', handler)
+  .on('unlink', handler)
+  .on('addDir', handler)
+  .on('unlinkDir', handler);
+
+console.log('文件监听器启动成功');
